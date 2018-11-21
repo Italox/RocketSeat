671d120e33b75051926901ccb54355b4721c3f52
Italo Aurelio <italoaurelior@gmail.com>
2018-11-21 06:42:23 -0200

    Create desafio.js

---
+++ ES6/Modulo 3/desafio.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+class Github {
+    static async getRepositories(repo) {  
+        try{     
+          const response = await axios.get(`https://api.github.com/repos/${repo}`);
+          console.log(response.data);
+        }
+        catch(err){
+          console.log('Repositório não existe');
+        }
+    }
+  }
+
+  Github.getRepositories('rocketseat/rocketseat.com.br');
+  Github.getRepositories('rocketseat/dslkvmskv');
+
+  const buscaUsuario = async usuario => {    
+      try {
+        const response = await axios.get(`https://api.github.com/users/${usuario}`)
+        console.log(response.data);
+      }
+      catch(err){
+        console.log('Usuário não existe');
+      }
+  }
+  buscaUsuario('italox');
